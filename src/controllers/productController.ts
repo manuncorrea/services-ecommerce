5a0ec60899ccc7af1c8f6462df8dac508f52bc92
Emanuele Correa <emanuelle@taqe.com.br>
2023-09-17 17:26:03 -0300

    adjustment without controller Product

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -4,11 +4,20 @@ import { ProductData, UpdatedProductData } from "../types/productTypes";
 const prisma = new PrismaClient();
 
 export const createProduct = async (productData: ProductData) => {
+  const category = await prisma.category.findUnique({ where: { id: productData.categoryId } });
+  if (!category) {
+    throw new Error('Category not found');
+  }
+
+  const brand = await prisma.brand.findUnique({ where: { id: productData.brandId } });
+  if (!brand) {
+    throw new Error('Brand not found');
+  }
+
   return prisma.product.create({
     data: productData
   });
 };
-
 export const getProduct = async (productId: string) => {
   return prisma.product.findUnique({
     where: {
