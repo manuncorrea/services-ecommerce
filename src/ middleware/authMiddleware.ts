a3801730c64fd5039f7eef1ee8037274064b7bbf
Emanuele Correa <emanuelle@taqe.com.br>
2023-10-07 15:39:24 -0300

    create middlware in products

---
+++ src/ middleware/authMiddleware.ts
@@ -0,0 +1,34 @@
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+
+interface TokenPayload {
+  userId: string;
+  iat: number;
+  exp: number;
+}
+
+export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).json({ message: 'Token not provided' });
+  }
+
+  const [, token] = authHeader.split(' ');
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY as string);
+
+    if (typeof decoded !== 'object' || decoded === null || !('userId' in decoded)) {
+      throw new Error('Token is invalid');
+    }
+
+    const tokenPayload = decoded as TokenPayload;
+
+    req.userId = tokenPayload.userId;
+
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: 'Invalid token' });
+  }
+};
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,6 +1,9 @@
 import express from 'express';
 import * as userRoutes from './userRoutes';
-import * as productRoutes from './productRoutes';  
+import * as productRoutes from './productRoutes';
+import * as categoryRoutes from './categoryRoutes';
+import * as brandRoutes from './brandRoutes';
+import { authMiddleware } from '../ middleware/authMiddleware';
 
 const router = express.Router();
 
@@ -9,10 +12,25 @@ router.post('/api/register', userRoutes.register);
 router.post('/api/login', userRoutes.login);
 
 // Product routes
-router.post('/api/products', productRoutes.create);
-router.get('/api/products/:id', productRoutes.get);
-router.put('/api/products/:id', productRoutes.update);
-router.delete('/api/products/:id', productRoutes.remove);
-router.get('/api/products', productRoutes.getAll);
+router.post('/api/products', authMiddleware, productRoutes.create); 
+router.get('/api/products/:id', productRoutes.get); 
+router.put('/api/products/:id', authMiddleware, productRoutes.update); 
+router.delete('/api/products/:id', authMiddleware, productRoutes.remove); 
+router.get('/api/products', productRoutes.getAll); 
+
+
+// Category routes
+router.post('/api/categories', categoryRoutes.create);
+router.get('/api/categories', categoryRoutes.getAll);
+router.get('/api/categories/:id', categoryRoutes.get);
+router.put('/api/categories/:id', categoryRoutes.update);
+router.delete('/api/categories/:id', categoryRoutes.remove);
+
+// Brand routes
+router.post('/api/brands', brandRoutes.create);
+router.get('/api/brands', brandRoutes.getAll);
+router.get('/api/brands/:id', brandRoutes.get);
+router.put('/api/brands/:id', brandRoutes.update);
+router.delete('/api/brands/:id', brandRoutes.remove);
 
 export default router;
